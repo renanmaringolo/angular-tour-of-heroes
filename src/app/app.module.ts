3c728931e7f57efefa9f6ac72ccdad6d4b1e8c0b
renanmaringolo <renan.maringolo@gmail.com>
2018-03-15 22:50:25 -0300

    msgs pvt & pub & services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HeroService } from './hero.service';
 import { FormsModule } from '@angular/forms'; // <--
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
 
 
 @NgModule({
   declarations: [
+
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    HeroService,
+    MessageService,
+  ],
+  bootstrap: [ AppComponent ]
+  
 })
 export class AppModule { }
